2a941b50d59243fb53ab309ae9cb03ae3e337246
gillHarjinder <laddivaliant.0001@gmail.com>
2018-08-10 20:11:54 -0700

    route file and Read me file has changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/**
+ * declaration: Components is added to this field.
+ * imports: use of libraries use here 
+ * providers: 
+ */
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
